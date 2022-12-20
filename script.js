a07dca4b4676ddd6ebcd150dfce6ffd3a3c47a3d
unknown <ledosnardo@gmail.com>
2022-12-19 21:39:45 -0300

    Termimei area de perguntas frequentes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const pergunta = document.querySelector("[data-pergunta]");
+const textos = document.querySelectorAll("[data-texto]");
 
 pergunta.addEventListener("click", (e) => {
     const texto = e.path[2].children[1];
 
-    texto.classList.toggle("ativo")
+    for(i = 0; i < textos.length; i++){
+       textos[i].classList.remove("ativo");
+    }
+
+    texto.classList.toggle("ativo");
 });
