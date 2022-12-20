068e4bee5d302fd2b753b85cb234bef74b95ad10
unknown <ledosnardo@gmail.com>
2022-12-19 22:43:24 -0300

    Otimizando a aba perguntas

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const textos = document.querySelectorAll("[data-texto]");
 
 pergunta.addEventListener("click", (e) => {
     const texto = e.path[2].children[1];
+    const classeTexto = texto.classList;
 
-    for(i = 0; i < textos.length; i++){
-       textos[i].classList.remove("ativo");
+    if(classeTexto[1] == "ativo"){
+        classeTexto.remove("ativo");
+
+    } else{
+        for(i = 0; i < textos.length; i++){
+            textos[i].classList.remove("ativo");
+        }
+
+        classeTexto.toggle("ativo");
     }
-
-    texto.classList.toggle("ativo");
 });
